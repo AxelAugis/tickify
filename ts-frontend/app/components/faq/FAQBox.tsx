867d155f2d21570a592271a98f5eb79c7ff9ae06
Axel Augis <augisax@gmail.com>
2025-04-14 22:51:41 +0200

    changed faq text colors

--- ts-frontend/app/components/faq/FAQBox.tsx
+++ ts-frontend/app/components/faq/FAQBox.tsx
@@ -11,7 +11,7 @@ interface FAQBoxProps {
 
 const FAQBox: React.FC<FAQBoxProps> = ({ item }) => {
     return (
-        <div className={`w-full rounded-lg bg-accent-dark-green text-accent-green `}>
+        <div className={`w-full rounded-lg bg-accent-dark text-accent-green `}>
             <button
                 onClick={item.onClick} 
                 className={`text-xl w-full flex items-center justify-between cursor-pointer p-5`}
@@ -26,7 +26,7 @@ const FAQBox: React.FC<FAQBoxProps> = ({ item }) => {
                 />
             </button>
             <div className={`w-full ${item.styles.content}`}>
-                <p className={`w-full px-5 pb-5 text-lg flex flex-col`}>
+                <p className={`w-full px-5 pb-5 text-lg flex flex-col text-light`}>
                   {
                     item.contents.map((content, index) => (
                         <span key={index} className={``}>
